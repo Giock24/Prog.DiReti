2bc7f37b2ea3ff9d9a3c7e98b44ea2a385ab6d1f
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-07-05 18:25:18 +0200

    Implemented method for getting list of Server's files

--- src/server/server.py
+++ src/server/server.py
@@ -1,39 +1,57 @@
 import os
+import sys
+import socket as sk
 
-from socket import *
+# this method return a list of all file that are present on Server
+def listingFiles():
+    lista = os.listdir(os.path.join(os.getcwd(), "serverStorage"))
+    return lista
 
-serverPort = 8080
-
-serverSocket = socket(AF_INET, SOCK_DGRAM);
+serverPort = 10000
+serverSocket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM);
 
 # nel primo paramentro di entrata presumo come IP : 127.0.0.1
 serverSocket.bind('',serverPort)
 
-serverSocket.listen(1)
-
 print('The server is Online on port: ', serverPort)
 
-while true:
-    # nella var connectioSocket viene messo un oggetto
-    # per poter comunicare con il client
+while True:
     
+    print("\n\r waiting to receive message...")
+    
+    # data = dato ricevuto dal client
     # addr = indirizzo del client
-    connectionSocket, addr = serverSocket.accept()
+    data, addr = serverSocket.recvfrom(4096)
+    
+    print('\n\r received %s bytes from %s' % (len(data), addr))
+    print(data.decode('utf8'))
     
     try:
         
-        message = connectionSocket.recv(4096)
-        
-        if len(message.split())>0:
-            print("")
+        if len(data)>0:
+            
+            
+            
+            if data == "ls":
+                lista = listingFiles()
+                for file in lista:
+                    sent = serverSocket.sendto(file.encode('utf8'), addr)
+                
+                responseServer='HTTP/1.1 200 OK List_Files_Sended'
+                sent = serverSocket.sendto(responseServer.encode(), addr)
+                print('\n\r sended this list %s' % lista.__str__())
+            else:
+                print("\n\r Command Not Found")
+            
         else:
-            print("The request is empty")
+            print("\n\r The message received is empty")
         
     except:
-        print("Something gone wrong")
+        print("\n\r Something gone wrong")
     
     
-    
+serverSocket.close()
+sys.exit()
     
     
     
